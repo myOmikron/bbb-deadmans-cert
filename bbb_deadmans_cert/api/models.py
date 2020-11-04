d40a4d200b02ed0c437be3cca1ddcd66b9334e28
myOmikron <accounts@omikron.dev>
2020-11-04 04:25:02 +0100

    Added basic models for users and meetings

--- bbb_deadmans_cert/api/models.py
+++ bbb_deadmans_cert/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db.models import IntegerField, ForeignKey, DateTimeField
 
-# Create your models here.
+
+class MeetingModel(models.Model):
+    meeting_id = IntegerField(default=-1)
+    start_time = DateTimeField()
+    stop_time = DateTimeField()
+
+
+class DeadmanStatusModel(models.Model):
+    client_id = IntegerField(default=-1)
+    meeting = ForeignKey(MeetingModel, on_delete=models.CASCADE)
