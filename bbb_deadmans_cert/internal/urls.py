6a58cda2918eced82e9f7a1971e1c08357e4c0fb
myOmikron <accounts@omikron.dev>
2020-11-04 04:26:24 +0100

    Added demo api calls for start and stopBreak as well as setNoDeadman

--- bbb_deadmans_cert/internal/urls.py
+++ bbb_deadmans_cert/internal/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from internal.views import *
 
 urlpatterns = [
-    path('isDeadmanEnabled', IsDeadmanEnabledView.as_view())
+    path('isDeadmanEnabled', IsDeadmanEnabledView.as_view()),
+    path('startBreak', StartBreakView.as_view()),
+    path('stopBreak', StopBreakView.as_view()),
+    path('setNoDeadman', SetNoDeadmanView.as_view())
 ]
--- bbb_deadmans_cert/internal/views.py
+++ bbb_deadmans_cert/internal/views.py
@@ -1,18 +1,53 @@
+from datetime import datetime
+
 from django.http import JsonResponse
 from django.views.generic import TemplateView
 
 
+def check_missing_parameter(get):
+    response = 200
+    data = {"success": False, "result": ""}
+    if "client_id" not in get:
+        data["result"] = "Parameter client_id is missing"
+        response = 400
+    if "meeting_id" not in get:
+        data["result"] = "Parameter meeting_id is missing"
+        response = 400
+    return data, response
+
+
 class IsDeadmanEnabledView(TemplateView):
     def get(self, request, *args, **kwargs):
-        response = 200
-        data = {"success": False, "result": ""}
-        if "client_id" not in request.GET:
-            data["result"] = "Parameter client_id is missing"
-            response = 400
-        if "meeting_id" not in request.GET:
-            data["result"] = "Parameter meeting_id is missing"
-            response = 400
+        data, response = check_missing_parameter(request.GET)
         if response == 200:
             data["success"] = True
             data["result"] = {"IsDeadmanEnabled": True}
         return JsonResponse(data, status=response, reason=data["result"])
+
+
+class StartBreakView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        data, response = check_missing_parameter(request.GET)
+        if response == 200:
+            data["success"] = True
+            data["result"] = "Break started at " + str(datetime.time(datetime.now())) + " UTC"
+        return JsonResponse(data, status=response, reason=data["result"])
+
+
+class StopBreakView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        data, response = check_missing_parameter(request.GET)
+        if response == 200:
+            data["success"] = True
+            data["result"] = "Break stopped at " + str(datetime.time(datetime.now())) + " UTC"
+        return JsonResponse(data, status=response, reason=data["result"])
+
+
+class SetNoDeadmanView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        data, response = check_missing_parameter(request.GET)
+        if response == 200:
+            data["success"] = True
+            data["response"] = "Pressed button at " + str(datetime.time(datetime.now())) + " UTC"
+        return JsonResponse(data, status=response, reason=data["result"])
+
