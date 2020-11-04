052846ee9ef8608ad9cb9ccc66050300a624d88c
myOmikron <accounts@omikron.dev>
2020-11-04 01:42:34 +0100

    Imported channels

--- bbb_deadmans_cert/bbb_deadmans_cert/settings.py
+++ bbb_deadmans_cert/bbb_deadmans_cert/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import channels
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,13 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
     'internal',
     'api'
 ]
